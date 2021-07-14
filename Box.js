eb37223fea2539e2e664689736a31cff12afaaa5
Anvi Agarwal <85110078+anviAgarwal@users.noreply.github.com>
2021-07-14 17:00:54 +0530

    Add files via upload

--- Box.js
+++ Box.js
@@ -1,28 +1,19 @@
-class Box
-{
-    constructor(x,y,width,height)
-    {
-        var option={
-            'restitution' : 0.9
+class Box {
+    constructor(x,y,width,height,color){
+        var options={
+            isStatic: true
         }
-        this.body=Bodies.rectangle(x,y,width,height,option);
+        this.body = Bodies.rectangle(x,y,width,height,options);
+        this.width = width;
+        this.height = height;
+        this.color = color;
+
         World.add(world,this.body);
-        
-        this.width=width;
-        this.height=height;
     }
 
-    display()
-    {
-        var pos=this.body.position;
-        var angle=this.body.angle;
-        push();
-        translate(pos.x,pos.y);
-        rotate(angle);
+    display() {
         rectMode(CENTER);
-        fill("green");
-        rect(0,0,this.width,this.height); 
-        pop();  
+        fill(this.color);
+        rect(this.body.position.x,this.body.position.y,this.width,this.height);
     }
 }
-
